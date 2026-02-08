5195856b6db17e42f1972b84d77efc7f2c3f5f39
Bolaji <daropaleb@gmail.com>
2026-02-08 16:35:15 +0100

    updates to movies app

--- src/movies.tsx
+++ src/movies.tsx
@@ -15,6 +15,7 @@ export default function Movies() {
     const [movies, setMovies] = useState<Movie[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
+    const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
     useEffect(() => {
         const fetchMovies = async () => {
@@ -51,7 +52,7 @@ export default function Movies() {
             <h1 className="text-5xl text-center mb-8 text-blue-500">Movies</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {movies.map((movie) => (
-                <div className="card bg-base-100 w-96 shadow-sm">
+                <div onClick={() => {setSelectedMovie(movie); document.getElementById('my_modal_1')?.showModal()}} className="card bg-base-100 w-96 shadow-sm">
             <figure>
              <img
       src={movie.Images[0]}
@@ -61,12 +62,61 @@ export default function Movies() {
     <h2 className="card-title">{movie.Title}</h2>
     <p>{movie.Plot}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-primary" onClick={() => setSelectedMovie(movie)}>Buy Now</button>
     </div>
   </div>
 </div>
             ))}
             </div>
+
+
+            {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+<dialog id="my_modal_1" className="modal">
+  <div className="modal-box">
+   
+    <div className="carousel rounded-box w-auto ">
+  <div className="carousel-item w-full">
+    <img
+      src={selectedMovie?.Images[0]}
+      className="w-full"
+      alt="Tailwind CSS Carousel component" />
+  </div>
+  <div className="carousel-item w-full">
+    <img
+      src={selectedMovie?.Images[1]}
+      className="w-full"
+      alt="Tailwind CSS Carousel component" />
+  </div>
+  <div className="carousel-item w-full">
+    <img
+      src={selectedMovie?.Images[2]}
+      className="w-full"
+      alt="Tailwind CSS Carousel component" />
+  </div>
+  <div className="carousel-item w-full">
+    <img
+      src={selectedMovie?.Images[3]}
+      className="w-full"
+      alt="Tailwind CSS Carousel component" />
+  </div>
+  
+</div>
+    <h3 className="font-bold text-lg">{selectedMovie?.Title}</h3>
+    <p className="py-4">{selectedMovie?.Plot}</p>
+    <p>Directed by: {selectedMovie?.Director}</p>
+    <p>Rated: {selectedMovie?.Rated}</p>
+    <p>Year: {selectedMovie?.Year}</p>
+    <div className="modal-action">
+      <form method="dialog">
+        {/* if there is a button in form, it will close the modal */}
+        <button className="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
+
+
         </div>
     )
 
